838ba372445ada8ef5d1e26616dbacb8139158dc
Adam Konber <konber3@gmail.com>
2024-03-22 14:41:29 -0600

    Complete meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem(name, price, type) {
+  return {
+    name: name,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(item, ingredients){ 
+  if(!ingredients.includes(item)){
+  return ingredients.push(item)
+} else {
+  return ingredients
+}
+ 
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(initialPrice) {
+return initialPrice * .9
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+var recipe = {
+  title: title,
+  ingredients: ingredients,
+  type: menuItemType, 
+}
+return recipe
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
 
