f2c2627e019bf5502eeed9381467ea42c4599ec7
Adam Konber <konber3@gmail.com>
2024-03-24 17:58:54 -0600

    Complete Order

--- src/order.js
+++ src/order.js
@@ -1,11 +1,10 @@
 function takeOrder(order, deliveryOrders){
   if(deliveryOrders.length < 3){
-  deliveryOrders.push(order)
+    deliveryOrders.push(order)
   }
-} 
+}; 
 
 //remove order that has the order number
-//
 
 function refundOrder(number, deliveryOrders) {
   // console.log(number, '<>>>>Number')
@@ -13,29 +12,28 @@ function refundOrder(number, deliveryOrders) {
   // console.log(deliveryOrders[0].orderNumber)
   for(var i=0; i < deliveryOrders.length; i++){
     if(number === deliveryOrders[i].orderNumber ){
-    return deliveryOrders.splice(i,1)
+      return deliveryOrders.splice(i,1)
+    }
   }
-}
-  }
+};
   
 function listItems(deliveryOrders) {
   // console.log('delivry<>>>>', deliveryOrders)
   var array = [];
   for(var i=0; i < deliveryOrders.length; i++) {
-  array.push(deliveryOrders[i].item)
+    array.push(deliveryOrders[i].item)
   }
   return array.join(', ')
-}
+};
 
 function searchOrder(order, deliveryOrders) { 
   for(var i = 0; i < order.length; i++){
-
     if(order[i].item === deliveryOrders) {
      return true;
     }
   }
   return false;
-}
+};
 
 
 module.exports = {
